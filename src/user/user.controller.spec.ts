5c28c4358864444463121798a7a9015ec04f3c02
fenghang <hang.feng@nx-engine.com>
2023-02-16 14:36:11 +0800

    Update Test

--- src/user/user.controller.spec.ts
+++ src/user/user.controller.spec.ts
@@ -1,12 +1,24 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from './entities/user.entity';
 import { UserController } from './user.controller';
+import { UserService } from './user.service';
+import envConfig from '../../config/env';
 
 describe('UserController', () => {
   let controller: UserController;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
+      imports: [
+        TypeOrmModule.forRoot({
+          ...envConfig.DATABASE_CONFIG,
+          entities: [User],
+        }),
+        TypeOrmModule.forFeature([User]),
+      ],
       controllers: [UserController],
+      providers: [UserService],
     }).compile();
 
     controller = module.get<UserController>(UserController);
